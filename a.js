b8ce0caaa29c498b941fbbe1b661eb00c816bcff
Karishma <karishma.7022@gmail.com>
2026-06-21 19:29:40 +0530

    learnt types in TS

---
+++ a.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("Karishma");
+//# sourceMappingURL=a.js.map
